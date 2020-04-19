c42c88113a1fc5f04aacc7a65406ac441624f307
Valeriya Vishnevskaya <valeriyavishnevskaya@MacBook-Pro-Valeriya.local>
2020-04-19 16:17:04 +0300

    add test_guest_should_see_login_link

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,6 +2,20 @@ from .base_page import BasePage
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
+
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    # def test_guest_should_see_login_link(browser):
+    #     link = "http://selenium1py.pythonanywhere.com/"
+    #     page = MainPage(browser, link)
+    #     page.open()
+    #     page.should_be_login_link()
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+
+
+
+ # $ git reset --hard a3775a5485af0af20375cedf46112db5f813322a
+ #    $ git push --force
